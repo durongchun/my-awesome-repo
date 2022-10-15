9926825dd2c76cbba310f809d87bcd44a94cbbf9
durongchun <durongchun@hotmail.com>
2022-10-15 10:41:36 -0700

    remove RunTestNg

--- src/test/java/com/holinova/testcase/AbcnullTest.java
+++ src/test/java/com/holinova/testcase/AbcnullTest.java
@@ -6,22 +6,22 @@ import com.holinova.pageobject.page.AbcnullPage;
 import org.testng.annotations.Test;
 
 /**
- * lucy 页面测试用例
+ * Lucy
  *
  * @author lucy
  * @version 1.0.0
- * @date 2020/1/28
+ * @date 2020/9/28
  */
 public class AbcnullTest extends BaseTest {
     private AbcnullPage abcnullPage;
 
     @Test(description = "abcnull页面_搜索测试", priority = 1)
     public void testSearch() {
-        // 初始化 lucy 页面
+        
         abcnullPage = new AbcnullPage(driver, redisUtil);
-        // 进入 lucy 首页
+        
         abcnullPage.enterPage();
-        // abncull 页面搜索检测
+        
         assert abcnullPage.search();
     }
 }
